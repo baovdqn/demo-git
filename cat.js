e7c4598b78f2d125ca11ffa014d171ac7dcecd1b
ha.leminh <ha.leminh@adamosoft.com>
2023-04-04 00:39:13 +0700

    update function cat contructor

--- cat.js
+++ cat.js
@@ -1,9 +1,13 @@
-function Cat(name){
+function Cat(name, color, age){
     this.name = name;
+    this.color = color;
+    this.age = age
 }
 Cat.prototype.sleep = () => console.log('mmmm');
 
 const tom = new Cat('tom')
 tom.sleep();
+
 console.log(tom)
+
 module.export = Cat;
